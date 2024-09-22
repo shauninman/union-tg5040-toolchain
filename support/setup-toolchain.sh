96c38b7430e4fb1eb987d09b675a574c021948fe
Shaun Inman <shaun@panic.com>
2024-09-22 18:35:38 -0400

    upgrade toolchain from linaro-7.5 to gcc-arm-8.3

--- support/setup-toolchain.sh
+++ support/setup-toolchain.sh
@@ -7,19 +7,19 @@ fi
 
 SYSROOT_TAR="SDK_usr_tg5040_a133p"
 TOOLCHAIN_NAME="aarch64-linux-gnu"
-TOOLCHAIN_TAR="$TOOLCHAIN_NAME-7.5.0-linaro"
-DOWNLOAD_URL="https://github.com/trimui/toolchain_sdk_smartpro/releases/download/20231018"
-TOOLCHAIN_URL="$DOWNLOAD_URL/$TOOLCHAIN_TAR.tgz"
-SYSROOT_URL="$DOWNLOAD_URL/$SYSROOT_TAR.tgz"
+TOOLCHAIN_TAR="gcc-arm-8.3-2019.02-x86_64-aarch64-linux-gnu"
+
+TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu-a/8.3-2019.02/$TOOLCHAIN_TAR.tar.xz"
+SYSROOT_URL="https://github.com/trimui/toolchain_sdk_smartpro/releases/download/20231018/$SYSROOT_TAR.tgz"
 
 cd ~
 
 wget $TOOLCHAIN_URL
 wget $SYSROOT_URL
 
-tar xf $TOOLCHAIN_TAR.tgz -C /opt
+tar xf $TOOLCHAIN_TAR.tar.xz -C /opt
 mv /opt/$TOOLCHAIN_TAR /opt/$TOOLCHAIN_NAME
-rm $TOOLCHAIN_TAR.tgz
+rm $TOOLCHAIN_TAR.tar.xz
 
 tar xf $SYSROOT_TAR.tgz
 rsync -a --ignore-existing ./usr/ /opt/$TOOLCHAIN_NAME/$TOOLCHAIN_NAME/libc/usr/
